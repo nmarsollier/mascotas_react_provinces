745aa44ee38803edbe964952e9982ba7d566e5e7
nmarsollier <nmarsollier@gmail.com>
2020-06-05 17:21:19 -0300

    Modulo provincias para mascotas

--- src/api/provincesApi.ts
+++ src/api/provincesApi.ts
@@ -1,7 +1,6 @@
-import axios from "axios";
 
-axios.defaults.headers.common["Content-Type"] = "application/json";
 import { environment } from "mascotas_react_common"
+import { securedAxios } from "mascotas_react_store";
 
 export interface Province {
     id: string;
@@ -10,7 +9,7 @@ export interface Province {
 
 export async function getProvinces(): Promise<Province[]> {
     try {
-        const res = await axios.get(environment.backendUrl + "/v1/province");
+        const res = await securedAxios().get(environment.backendUrl + "/v1/province");
         return Promise.resolve(res.data);
     } catch (err) {
         return Promise.reject(err);
